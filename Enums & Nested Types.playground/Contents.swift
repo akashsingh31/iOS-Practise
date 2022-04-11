1c818dfc6866692fc17d3c0942588c1c61a81b26
akash <akashsingh31march@gmail.com>
2022-04-11 16:31:00 +0530

    New file - Second commit

---
+++ Enums & Nested Types.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+/*
+//Enums - are nothing but states or types of an object can have
+
+--------
+ Code 1
+--------
+struct Employee{
+    var name: String!
+    var role: employeeRole!
+}
+
+enum employeeRole : String {
+    case contributor = "Contributor"
+    case admin = "Admin"
+}
+
+let employee1 = Employee(name: "Akash", role: .contributor)
+let employee2 = Employee(name: "Sneh", role: .admin)
+
+print(employee2.role.rawValue)
+
+--------
+ Code 2
+--------
+struct students{
+    var name: String!
+    var status: employmentStatus!
+}
+
+enum employmentStatus : String{
+    case placed = "PLACED"
+    case notplaced = "NOT-PLACED"
+}
+
+let student1 = students(name: "AKASH", status: .placed)
+let student2 = students(name: "SNEH", status: .placed)
+let student3 = students(name: "XYZ", status: .notplaced)
+
+print(student1.status.rawValue)
+print(student2.status.rawValue)
+print(student3.status.rawValue)
+ */
+
+//Initialization  
